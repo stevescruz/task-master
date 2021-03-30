55f89666c59ca8465335e45765cb4de1668b9bd4
unknown <steve.silverio.cruz@gmail.com>
2021-03-30 18:00:08 -0300

    FEAT: Added task command to CLI

--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,18 @@
 const { Command } = require('commander');
 
-function main() {
+const { makeTaskCommand } = require('./commands/makeTaskCommand');
+
+async function main() {
     const program = new Command;
 
     program
         .version('1.0.0', '-v, --version', "output Task Master's current version")
         .description('Task Master is a command-line todo list that increases your productivity by reducing your distractions')
+        .addCommand(await makeTaskCommand())
         .allowUnknownOption(false)
         .allowExcessArguments(false);
 
-    program.parse(process.argv);
+        program.parse(process.argv);
 }
 
 main();
