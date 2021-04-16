2919f504d4c8fb9b093c60aaa2c65f047408afc7
unknown <steve.silverio.cruz@gmail.com>
2021-04-16 02:04:28 -0400

    REF: Task Model refactored to throw an error when not provided with id or description

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,5 +1,12 @@
 class Task {
     constructor({ id, description, status, age, priority }) {
+        if(!id) {
+            throw new Error('To register a task you must provide a valid id.')
+        }
+        if(!description) {
+            throw new Error('To register a task you must provide a valid description.')
+        }
+
         this.id = id;
         this.description = description;
         this.status = status || 'pending';
