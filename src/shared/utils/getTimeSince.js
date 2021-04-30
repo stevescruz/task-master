0d6ae1e7fb164d731404a707b3fcf099ca27c4f5
stevescruz <steve.silverio.cruz@gmail.com>
2021-04-29 22:31:28 -0400

    REF: getTimeSince method to display abbreviation for time intervals

--- src/shared/utils/getTimeSince.js
+++ src/shared/utils/getTimeSince.js
@@ -7,39 +7,39 @@ function getTimeSince(date) {
 
     if (interval > 1) {
         const years = Math.floor(interval); 
-        return `${years} year${years !== 1 ? 's' : ''}`;
+        return `${years}y`;
     }
 
     interval = seconds / 2628288;
 
     if (interval > 1) {
         const months = Math.floor(interval);
-        return `${months} month${months !== 1 ? 's' : ''}`;
+        return `${months}m`;
     }
 
     interval = seconds / 86400;
 
     if (interval > 1) {
         const days = Math.floor(interval);
-        return `${days} day${days !== 1 ? 's' : ''}`;
+        return `${days}d`;
     }
 
     interval = seconds / 3600;
 
     if (interval > 1) {
         const hours = Math.floor(interval);
-        return `${hours} hour${hours !== 1 ? 's' : ''}`;
+        return `${hours}h`;
     }
 
     interval = seconds / 60;
 
     if (interval > 1) {
         const minutes = Math.floor(interval);
-        return `${minutes} minute${minutes !== 1 ? 's' : ''}`;
+        return `${minutes}min`;
     }
 
     seconds = Math.floor(seconds);  
-        return `${seconds} second${seconds !== 1 ? 's' : ''}`;
+        return `${seconds}s`;
 }
 
 module.exports = getTimeSince;
