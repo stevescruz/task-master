269a04eda1a9bb769bc0d478f303b72846f8caaf
stevescruz <steve.silverio.cruz@gmail.com>
2021-04-30 00:49:31 -0400

    REF: TasksController stopped using the createTable method -Refactored the index method to use the renderTasks method. -Refactored the index method to receive the status and tag properties to control with tasks are displayed. -create, delete and index methods do not display tasks through table anymore.

--- src/controllers/TasksController.js
+++ src/controllers/TasksController.js
@@ -2,9 +2,8 @@ const CreateTaskService = require('../services/CreateTaskService');
 const DeleteTaskService = require('../services/DeleteTaskService');
 const ListTasksService = require('../services/ListTasksService');
 
-const selectObjectProperties = require('../shared/utils/selectObjectProperties');
 const joinInput = require('../shared/utils/joinInput');
-const createTable = require('../shared/utils/createTable');
+const renderTasks = require('../shared/utils/renderTasks');
 const askForConfirmation = require('../shared/utils/askForConfirmation');
 
 const MessageColorEnum = require('../shared/enums/MessageColorEnum');
@@ -19,15 +18,11 @@ class TasksController {
             const parsedDescription = joinInput(description, ' ');
 
             const createTask = new CreateTaskService(this.tasksRepository);
-            const task = await createTask.execute({
+            await createTask.execute({
                 description: parsedDescription,
                 priority: priorityOption,
             });
-            const properties = ['id', 'description', 'status'];
-            const selectedTask = selectObjectProperties(properties, task);
 
-            const table = createTable(properties, [selectedTask]);
-            console.log(table);
             console.log(MessageColorEnum.SUCCESS(`New task added successfully.`));
         } catch (error) {
             console.error(MessageColorEnum.ERROR(error.message));
@@ -48,13 +43,8 @@ class TasksController {
             if(answer) {
                 const deleteTask = new DeleteTaskService(this.tasksRepository);
 
-                const task = await deleteTask.execute(parsedId)
+                await deleteTask.execute(parsedId)
     
-                const properties = ['id', 'description', 'age', 'status'];
-                const selectedTask = selectObjectProperties(properties, task);
-    
-                const table = createTable(properties, [selectedTask]);
-                console.log(table);
                 console.log(MessageColorEnum.SUCCESS('Task deleted successfully.'));
             } else {
                 console.log(MessageColorEnum.WARNING('Operation aborted.'));
@@ -64,21 +54,12 @@ class TasksController {
         }
     }
     
-    async index(showAllOption) {
+    async index(status, tag) {
         try {
-            const showAll = showAllOption ? true : false;
             const listTasks = new ListTasksService(this.tasksRepository);
-            const tasks = await listTasks.execute(showAll);
+            const tasks = await listTasks.execute(status, tag);
 
-            const properties = ['id', 'description', 'age', 'priority', 'status'];
-            const mappedTasks = tasks.map(task => {
-                const selectedTask = selectObjectProperties(properties, task);
-                return selectedTask;
-            });
-
-            const table = createTable(properties, mappedTasks);
-            console.log(table);
-            console.log(MessageColorEnum.SUCCESS(`${mappedTasks.length} ${mappedTasks.length !== 1 ? 'tasks were' : 'task was'} displayed successfully.`));
+            renderTasks(tasks, tag);
         } catch (error) {
             console.error(MessageColorEnum.ERROR(error.message));
         }
