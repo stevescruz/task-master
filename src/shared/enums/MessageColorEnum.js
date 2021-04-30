1b81e5fa15d61df517bc194c47ede2387eadc71f
stevescruz <steve.silverio.cruz@gmail.com>
2021-04-30 02:35:00 -0400

    REF: MessageColorEnum SUCCESS, WARNING, ERROR and priorities messages colors changed

--- src/shared/enums/MessageColorEnum.js
+++ src/shared/enums/MessageColorEnum.js
@@ -4,22 +4,22 @@ const { supportsColor } = require('chalk');
 const { enableTerminalColors } = require('../../config/config.json'); 
 
 MessageColorEnum = {
-    SUCCESS: chalk.bold.green,
-    WARNING: chalk.bold.hex('#FFA500'),
-    ERROR: chalk.bold.red,
+    SUCCESS: chalk.bold.hex('#05c46b'),
+    WARNING: chalk.bold.hex('#ffd32a'),
+    ERROR: chalk.bold.hex('#ef5777'),
     
-    LOW_PRIORITY: chalk.yellowBright,
-    NORMAL_PRIORITY: chalk.hex('#FFA500'),
-    HIGH_PRIORITY: chalk.redBright,
+    LOW_PRIORITY: chalk.hex('#ffda79'),
+    NORMAL_PRIORITY: chalk.hex('#ffa500'),
+    HIGH_PRIORITY: chalk.hex('#ff5252'),
 
     BOARD: chalk.underline.hex('#d2dae2'),
     STATISTICS: chalk.hex('#808e9b'),
     DESCRIPTION: chalk.hex('#d2dae2'),
     TIME_STAMP: chalk.hex('#808e9b'),
     CHECKED: chalk.hex('#808e9b'),
-    UNTICKED: chalk.hex('#FC427B'),
+    UNTICKED: chalk.hex('#f53b57'),
     TICK: chalk.hex('#0be881'),
-    NOTE: chalk.hex('#1B9CFC'),
+    NOTE: chalk.hex('#1b9cfc'),
 }
 
 chalk.level = enableTerminalColors ? supportsColor.level : 0;
