db1283d99eeff685d8942ccb68cfc5114e2094ee
stevescruz <steve.silverio.cruz@gmail.com>
2021-05-04 22:23:31 -0400

    REF: TableColorEnum.js removed since it is not necessary anymore

--- src/shared/enums/TableColorEnum.js
+++
@@ -1,9 +0,0 @@
-const { enableTerminalColors } = require('../../config/config.json'); 
-
-TableColorEnum = {
-    HEADER: enableTerminalColors ? 'red' : 'white',
-    ROWS: 'white',
-    ID: enableTerminalColors ? 'red' : 'white',
-}
-
-module.exports = TableColorEnum;
