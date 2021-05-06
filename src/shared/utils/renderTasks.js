ade61e16d6fdca286ae11a8d04e5ccd16ab14041
stevescruz <steve.silverio.cruz@gmail.com>
2021-05-06 18:27:26 -0400

    REF: renderTasks method now displays priorities for pending tasks

--- src/shared/utils/renderTasks.js
+++ src/shared/utils/renderTasks.js
@@ -1,17 +1,33 @@
-const { BOARD, STATISTICS, DESCRIPTION, TIME_STAMP, CHECKED, UNTICKED, TICK, NOTE } = require('../enums/MessageColorEnum');
+const { 
+  BOARD,
+  STATISTICS,
+  DESCRIPTION,
+  TIME_STAMP,
+  CHECKED,
+  UNTICKED,
+  TICK,
+  LOW_PRIORITY,
+  NORMAL_PRIORITY,
+  HIGH_PRIORITY,
+} = require('../enums/MessageColorEnum');
 
 function renderTasks(tasks, title) {
 
   const itemSymbol = {
     pending: UNTICKED('☐'),
     done: TICK('✓'),
-    note: NOTE('•'),
   };
 
   const descriptionColor = {
     done: CHECKED,
     pending: DESCRIPTION,
   }
+
+  const priorityColor = {
+    L: `${CHECKED(' (low)')}`,
+    N: `${NORMAL_PRIORITY('')}`,
+    H: `${HIGH_PRIORITY(' (!)')}`,
+  };
 
   let completedTasks = 0;
   tasks.forEach(task => {
@@ -20,7 +36,7 @@ function renderTasks(tasks, title) {
     }
   });
 
-  const boardName = title ? `@${title}` : '@tasks';
+  const boardName = title ? `${title}` : 'tasks';
 
   const totalTasks = tasks.length;
   const pendingTasks = totalTasks - completedTasks;
@@ -28,17 +44,17 @@ function renderTasks(tasks, title) {
 
   console.log(`${BOARD(boardName)} ${STATISTICS(completedTasksFraction)}`);
   if (tasks.length !== 0) {
-    const mappedTasks = tasks.map(task => `  ${STATISTICS(`${task.id}.`)} ${itemSymbol[task.status]} ${descriptionColor[task.status](task.description)} ${TIME_STAMP(task.age)}`);
+    const mappedTasks = tasks.map(task => `  ${STATISTICS(`${task.id}.`)} ${itemSymbol[task.status]} ${descriptionColor[task.status](task.description)} ${TIME_STAMP(task.age)}${task.status === 'pending' ? priorityColor[task.priority] : ''}`);
 
     mappedTasks.map(task => console.log(task));
 
     const percentage = +((completedTasks * 100) / tasks.length).toFixed(2);
-    const statisticsMessage = `${percentage}% of tasks completed.`;
+    const statisticsMessage = `${percentage}% completed`;
 
     console.log(`\n${STATISTICS(statisticsMessage)}`);
   }
-  
-  console.log(`${TICK(completedTasks)} ${STATISTICS(`done -`)} ${UNTICKED(pendingTasks)} ${STATISTICS(`pending -`)} ${NOTE('0')} ${STATISTICS('notes')}`);
+
+  console.log(`${TICK(completedTasks)} ${STATISTICS(`done -`)} ${UNTICKED(pendingTasks)} ${STATISTICS(`pending`)}`);
 }
 
 module.exports = renderTasks;
